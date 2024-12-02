5ca4ca0525fd228495924989f91096910b686a61
leomunsa <leo_mc_inter@hotmail.com>
2024-12-02 00:23:24 -0300

    start the integration with inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}`).default,
+  setup({ el, App, props }) {
+    createRoot(el).render(<App {...props} />);
+  },
+});
+
